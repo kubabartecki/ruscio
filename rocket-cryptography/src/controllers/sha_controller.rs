ba6093b55af3fa5c914cb55c1e7f8c96f9fd400f
kubabartecki <169511@stud.prz.edu.pl>
2024-05-22 20:56:06 +0200

    update sha256/hash endpoint

--- rocket-cryptography/src/controllers/sha_controller.rs
+++ rocket-cryptography/src/controllers/sha_controller.rs
@@ -1,19 +1,9 @@
-use rocket::response::status::Custom;
-use rocket::serde::{Serialize, Deserialize, json::Json};
+use rocket::serde::{json::Json};
+use crate::services::sha_service::ShaService;
+use crate::models::{ShaRequest, ShaResponse};
 
-#[derive(Deserialize)]
-struct ShaInput {
-    input: String,
-}
-
-#[derive(Serialize)]
-struct ShaOutput {
-    hash: String,
-}
-
-#[post("/sha256/hash", format = "json", data = "<input>")]
-pub fn hash(input: Json<ShaInput>) -> Result<Json<ShaOutput>, Custom<String>> {
-    Ok(Json(ShaOutput {
-        hash: "hash".to_string(),
-    }))
+#[post("/sha256/hash", format = "json", data = "<request>")]
+pub async fn hash(request: Json<ShaRequest>) -> Json<ShaResponse> {
+    let response = ShaService::hash(request.into_inner());
+    Json(response)
 }
