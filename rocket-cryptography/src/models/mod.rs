f554540074cb08e50e1eb5fa3c7a97aaacdeadea
kubabartecki <169511@stud.prz.edu.pl>
2024-05-24 18:28:43 +0200

    add hash model

--- rocket-cryptography/src/models/mod.rs
+++ rocket-cryptography/src/models/mod.rs
@@ -1,7 +1,9 @@
 pub mod aes;
 pub mod rsa;
 pub mod sha;
+pub mod hash;
 
 pub use aes::{AesKey, AesPlaintext, AesCiphertext};
 pub use rsa::{RsaKey, RsaPlaintext, RsaCiphertext};
 pub use sha::{ShaRequest, ShaResponse};
+pub use hash::{Hash, NewHash, HashRequest};
